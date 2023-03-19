45c7462820cdd9c26e2bba325eab0da10824429d
Diego Araujo <diegozigoto@gmail.com>
2023-03-19 12:14:20 -0300

    Initial commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,43 @@
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+        private static List<Filme> filmes = new List<Filme>();
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmesById),
+                new { id = filme.Id },filme); 
+        }
+
+        [HttpGet("id")]
+        public IActionResult RecuperaFilmesById(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null) return NotFound();
+            return Ok(filme);  
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> FilmesPaginate([FromQuery]int skip, [FromQuery] int take)
+        {
+            return _context.Filmes.Skip(skip).Take(take);
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage = "O título é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Gênero é obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duracao dever ser entre 70 minutos e 10 horas")]
+        public string Duracao { get; set; }
+        
+    }
+}
